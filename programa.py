631432052677130af851e7e24c7b338193748ec8
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-11-04 16:52:38 -0300

    linhas

--- programa.py
+++ programa.py
@@ -12,24 +12,34 @@ while True:
     print("0 - Sair")
 
     op=int(input("Digite sua escolha: "))
+    print('-------------------------------------------------------')     
+
 
     if op==1:
         print(registrar_viagem(listaViagens))
+        print('-------------------------------------------------------')     
 
     elif op==2:
         print(exibir_viagens(listaViagens))
+        print('-------------------------------------------------------')     
 
     elif op==3:
         print(buscar_motorista(listaViagens))
+        print('-------------------------------------------------------')     
 
     elif op==4:
         print(viagem_mais_cara(listaViagens))
+        print('-------------------------------------------------------')     
 
     elif op==5:
         print(media_consumo(listaViagens))
+        print('-------------------------------------------------------')     
 
     elif op==0:
         print("saindo do controle de viagens...")
+        print('-------------------------------------------------------')     
+
         break
     else:
-        print("escolhe direito")        
+        print("escolhe direito")
+        print('-------------------------------------------------------')     
